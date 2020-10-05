ad1179dd5cc7b1c437ce41516ccfd46de08af287
tatsuya <t2h5k0@gmail.com>
2020-10-05 20:03:15 +0900

    fix bootstrap.sh

--- internal/bootstrap.sh
+++ internal/bootstrap.sh
@@ -6,3 +6,5 @@ git grep -l 'rails-api-template' -- ':!internal' \
   | xargs sed -i '' -e "s/rails-api-template/$project/g"
 git grep -l 'rails_api_template' -- ':!internal' \
   | xargs sed -i '' -e "s/rails_api_template/$(echo $project | tr '-' '_')/g"
+git grep -l 'RailsApiTemplate' -- ':!internal' \
+  | xargs sed -i '' -e "s/RailsApiTemplate/$(echo $project | gsed -r 's/(^|-)(.)/\U\2\E/g')/g"
